8bc8309031889609a3a3bf97d8f70788d2cfaa50
Juliano Gomes Sousa <julianosousa@ibm.com>
2022-02-21 12:45:39 -0300

    initial commit

---
+++ fornecedor-service/src/main/java/com/micro/fornecedorservice/FornecedorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.fornecedorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorServiceApplication.class, args);
+	}
+
+}
---
+++ fornecedor-service/src/test/java/com/micro/fornecedorservice/FornecedorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.fornecedorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
